848dfc02bd35d51c277f44f7069be63f6cec0b1e
portablecontractor live <yeos.oak@gmail.com>
2013-10-18 17:17:17 +0000

    Moorman live

--- module/Application/config/routes.config.php
+++ module/Application/config/routes.config.php
@@ -3,15 +3,26 @@ return array(
     'router' => array(
         'routes' => array(
             'home' => array(
+                        'type' => 'literal',
+                'options' => array(
+                    'route' => '/',
+                    'defaults' => array(
+                    '__NAMESPACE__' => 'Application\Controller',
+                    'controller'    => 'Index',
+                    'action'        => 'index',
+                    ),
+                ),
+            ),
+            'thanks' => array(
                 'type' => 'literal',
-		'options' => array(
-		    'route' => '/',
-		    'defaults' => array(
-			'__NAMESPACE__' => 'Application\Controller',
-			'controller'    => 'Index',
-			'action'        => 'index',
-		    ),
-		),
+                'options' => array(
+                    'route' => '/thanks',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Application\Controller',
+                        'controller'    => 'Thanks',
+                        'action'        => 'index',
+                    ),
+                ),
             ),
         ),
     ),
