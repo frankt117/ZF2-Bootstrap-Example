cb8f6ff6198df3d6eeef0f9c14a775e8b6049782
Dustin Moorman <dustin.moorman@gmail.com>
2013-11-09 18:29:00 -0600

    Triple Doubles, fixin spaces in thys shyt.

--- module/Application/config/controllers.config.php
+++ module/Application/config/controllers.config.php
@@ -1,13 +1,13 @@
 <?php
 return array(
     'invokables' => array(
-	'Application\Controller\Index' => 'Application\Controller\IndexController',
-    'Application\Controller\Thanks'=> 'Application\Controller\ThanksController',
-    'Application\Controller\Affiliate' => 'Application\Controller\AffiliateController',
-    'Application\Controller\Carports' => 'Application\Controller\CarportsController',
-    'Application\Controller\Garages' => 'Application\Controller\GaragesController',
-    'Application\Controller\Buildings' => 'Application\Controller\BuildingsController',
-    'Application\Controller\Contact' => 'Application\Controller\ContactController',
-    'Application\Controller\GetStarted' => 'Application\Controller\GetStartedController'
+        'Application\Controller\Index' => 'Application\Controller\IndexController',
+        'Application\Controller\Thanks'=> 'Application\Controller\ThanksController',
+        'Application\Controller\Affiliate' => 'Application\Controller\AffiliateController',
+        'Application\Controller\Carports' => 'Application\Controller\CarportsController',
+        'Application\Controller\Garages' => 'Application\Controller\GaragesController',
+        'Application\Controller\Buildings' => 'Application\Controller\BuildingsController',
+        'Application\Controller\Contact' => 'Application\Controller\ContactController',
+        'Application\Controller\GetStarted' => 'Application\Controller\GetStartedController'
     ),
 );
