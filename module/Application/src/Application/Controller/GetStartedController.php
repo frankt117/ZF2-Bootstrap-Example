4fa362d27249934a0a0fe3eaa5f3b86b0031921a
portablecontractor live <yeos.oak@gmail.com>
2013-11-11 22:03:36 +0000

    Prepping for persistence

--- module/Application/src/Application/Controller/GetStartedController.php
+++ module/Application/src/Application/Controller/GetStartedController.php
@@ -3,12 +3,27 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Doctrine\ORM\EntityManager;
+use Application\Entity\Requests;
 
 class GetStartedController extends AbstractActionController
 {
+	protected $em;
+    
+    protected function getEntityManager(){
+
+	if(null === $this->em){
+		$this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+	}
+
+	return $this->em;
+
+    }
+
+
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel(array('records' => $this->getEntityManager()->getRepository('Application\Entity\Requests')));
     }
 
     public function estimateAction()
