ead8108ff2c3e9b06a04749755edc7bb40ba8208
Dustin Moorman <dustin.moorman@gmail.com>
2013-11-17 11:22:21 -0600

    Initial Category Page Cleanup

--- module/Application/src/Application/Controller/CategoryController.php
+++ module/Application/src/Application/Controller/CategoryController.php
@@ -10,8 +10,41 @@ class CategoryController extends AbstractActionController
     {
         return new ViewModel(
             array(
-                'category_slug' => $this->getEvent()->getRouteMatch()->getParam('category_slug')
+                'category_title'            => $this->getCategoryTitle(),            // for now its the slug
+                'description'               => $this->getCategoryDescription(),
+                'category_image_directory'  => $this->getCategoryImageDirectory(),
+                'category_slug'             => $this->getCategorySlug()
             )
         );
+    }
+
+    protected function getCategoryImageDirectory(){
+        return APPLICATION_ROOT. '/img/' . $this->getCategorySlug();
+    }
+
+    /**
+     * @TODO Replace this with a doctrine entity call to get description.
+     * @return string
+     */
+    protected function getCategoryDescription(){
+        return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat porttitor felis, et pulvinar quam
+        convallis eget. Integer pulvinar imperdiet erat. Donec porttitor sagittis erat eu vestibulum. Fusce vehicula mollis
+        leo. Nulla at placerat tellus, eu euismod arcu. Donec porttitor, felis quis malesuada tempor, mi justo feugiat orci,
+        vitae accumsan enim elit sed purus. Curabitur rutrum, lectus at fermentum auctor, sem massa placerat ligula,
+        id dignissim nisl augue eu sapien. Suspendisse euismod massa nec commodo hendrerit. Etiam vehicula viverra sapien,
+        rhoncus auctor ante cursus nec. Quisque nisl turpis, lobortis at nisl in, pharetra auctor diam. Phasellus et vulputate
+        purus. Morbi ut risus in nisi rhoncus posuere.';
+    }
+
+    /**
+     * @TODO for now, replace this with call to doctrine for appropriate category specific title.
+     * @return string
+     */
+    protected function getCategoryTitle(){
+        return ucwords(str_replace("-", " ", $this->getCategorySlug()));
+    }
+
+    protected function getCategorySlug(){
+        return $this->getEvent()->getRouteMatch()->getParam('category_slug');
     }
 }
