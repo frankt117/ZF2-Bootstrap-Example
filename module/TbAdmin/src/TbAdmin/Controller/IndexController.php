445c639a9022ce0d027c46ae971669f5c5c6f0ca
dustinmoorman <dustin.moorman@gmail.com>
2013-11-18 13:42:08 -0600

    Fixing that Cottage Goof.

--- module/TbAdmin/src/TbAdmin/Controller/IndexController.php
+++ module/TbAdmin/src/TbAdmin/Controller/IndexController.php
@@ -27,14 +27,14 @@ class IndexController extends AbstractActionController
     public function updateAction(){
 
         $em = $this->getEntityManager();
-        $PtgTbCategory = new \PtgTbCategory\Entity\Category();
-
+//        $PtgTbCategory = new \PtgTbCategory\Entity\Category();
+        $PtgTbCategory   = $em->getRepository('\PtgTbCategory\Entity\Category')->findOneBy(array('slug' => 'Cottages'));
         $PtgTbCategory->description = "Whether it’s for your bed and breakfast, or you are needing the extra room for the in-laws, our high-quality cottages are built with the style you want at a very affordable price. From being 100% American made, to offering a 10 year warranty, we are positive we can design the perfect cottage for your needs. Contact us today for pricing and details.";
 
         $PtgTbCategory->title = "Cottages";
         $PtgTbCategory->image_directory = 'cottages';
-        $PtgTbCategory->main_pic_src = 'garage1.jpg';
-        $PtgTbCategory->slug = "Cottages";
+        $PtgTbCategory->main_pic_src = 'cottage6.jpg';
+        $PtgTbCategory->slug = "cottages";
         $PtgTbCategory->subdescription = "Styled for you at an affordable price.";
 
 
