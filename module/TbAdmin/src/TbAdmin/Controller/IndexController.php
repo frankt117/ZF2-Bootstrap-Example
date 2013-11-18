c96c22d9e199dbbf5fd234c9b68c6664b16ea60d
dustinmoorman <dustin.moorman@gmail.com>
2013-11-18 14:00:51 -0600

    commenting out category creation script.

--- module/TbAdmin/src/TbAdmin/Controller/IndexController.php
+++ module/TbAdmin/src/TbAdmin/Controller/IndexController.php
@@ -26,19 +26,19 @@ class IndexController extends AbstractActionController
 
     public function updateAction(){
 
-        $em = $this->getEntityManager();
-        $PtgTbCategory = new \PtgTbCategory\Entity\Category();
-        $PtgTbCategory->description = "Looking for extra space for your tools and equipment, or the perfect spot for your ultimate man-cave? Our highly-customizable shops give you the space you need at an affordable price. From being 100% American made, to offering a 10 year warranty, we are positive we can build the perfect shop for your needs. Contact us today for pricing and details.";
-
-        $PtgTbCategory->title = "Shops";
-        $PtgTbCategory->image_directory = 'shops';
-        $PtgTbCategory->main_pic_src = 'shop.jpg';
-        $PtgTbCategory->slug = "shops";
-        $PtgTbCategory->subdescription = "The perfect choice for storing your tools and equipment, or building your ultimate man-cave.";
-
-
-        $em->persist($PtgTbCategory);
-        $em->flush();
+//        $em = $this->getEntityManager();
+//        $PtgTbCategory = new \PtgTbCategory\Entity\Category();
+//        $PtgTbCategory->description = "Looking for extra space for your tools and equipment, or the perfect spot for your ultimate man-cave? Our highly-customizable shops give you the space you need at an affordable price. From being 100% American made, to offering a 10 year warranty, we are positive we can build the perfect shop for your needs. Contact us today for pricing and details.";
+//
+//        $PtgTbCategory->title = "Shops";
+//        $PtgTbCategory->image_directory = 'shops';
+//        $PtgTbCategory->main_pic_src = 'shop.jpg';
+//        $PtgTbCategory->slug = "shops";
+//        $PtgTbCategory->subdescription = "The perfect choice for storing your tools and equipment, or building your ultimate man-cave.";
+//
+//
+//        $em->persist($PtgTbCategory);
+//        $em->flush();
 
     }
 }
