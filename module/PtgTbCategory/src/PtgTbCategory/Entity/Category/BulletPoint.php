63d3a1202c5faf31193cc98c7dd608c813c79664
Dustin Moorman <dustin.moorman@gmail.com>
2013-11-17 21:50:33 -0600

    Adding ID and cleanup.

--- module/PtgTbCategory/src/PtgTbCategory/Entity/Category/BulletPoint.php
+++ module/PtgTbCategory/src/PtgTbCategory/Entity/Category/BulletPoint.php
@@ -7,9 +7,18 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity (repositoryClass="\PtgTbCategory\Repository\Category\BulletPoint")
  * @ORM\Table(name="ptgtbcategory_category_bulletpoints")
  * @PtgBase\Doctrine\DiscriminatorEntry(value="PtgTbCategory\Entity\Category\BulletPoint")
+ * @property string $text
+ * @property int $id
  */
 class BulletPoint
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer");
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
     /**
      * @ORM\ManyToOne(targetEntity="PtgTbCategory\Entity\Category", inversedBy="BulletPoints")
      * @ORM\JoinColumn(name="ptgtbcategory_category_id", referencedColumnName="id")
