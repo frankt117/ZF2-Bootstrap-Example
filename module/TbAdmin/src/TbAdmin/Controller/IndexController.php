8651bad1f7bc6efe8180419f0289e225d8ab9572
dustinmoorman <dustin.moorman@gmail.com>
2013-11-18 07:23:34 -0600

    Removing Barn data addition.

--- module/TbAdmin/src/TbAdmin/Controller/IndexController.php
+++ module/TbAdmin/src/TbAdmin/Controller/IndexController.php
@@ -27,21 +27,21 @@ class IndexController extends AbstractActionController
     public function updateAction(){
 
         $em = $this->getEntityManager();
-        $PtgTbCategory = new \PtgTbCategory\Entity\Category();
-
-        $PtgTbCategory->description = 'Easily being our most popular items, our barns accommodate everything from horses, to livestock, to
-       even RV’s. With our wide variety and multiple sizes, we can build the ultimate barn to your specifications.
-       With our 10 year warranty and our barns being 100% American made, we are the perfect choice for you.';
-
-        $PtgTbCategory->title = "Barns";
-        $PtgTbCategory->image_directory = 'barns';
-        $PtgTbCategory->main_pic_src = 'building.jpg';
-        $PtgTbCategory->slug = "barns";
-        $PtgTbCategory->subdescription = "Affordable and built durable for your horses, livestock, and RV.  ";
-
-
-        $em->persist($PtgTbCategory);
-        $em->flush();
+//        $PtgTbCategory = new \PtgTbCategory\Entity\Category();
+//
+//        $PtgTbCategory->description = 'Easily being our most popular items, our barns accommodate everything from horses, to livestock, to
+//       even RV’s. With our wide variety and multiple sizes, we can build the ultimate barn to your specifications.
+//       With our 10 year warranty and our barns being 100% American made, we are the perfect choice for you.';
+//
+//        $PtgTbCategory->title = "Barns";
+//        $PtgTbCategory->image_directory = 'barns';
+//        $PtgTbCategory->main_pic_src = 'building.jpg';
+//        $PtgTbCategory->slug = "barns";
+//        $PtgTbCategory->subdescription = "Affordable and built durable for your horses, livestock, and RV.  ";
+//
+//
+//        $em->persist($PtgTbCategory);
+//        $em->flush();
 
 
         $allrecs = $em->getRepository('\PtgTbCategory\Entity\Category')->findAll();
