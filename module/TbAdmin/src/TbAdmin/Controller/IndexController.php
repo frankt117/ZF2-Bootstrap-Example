13d82d0fbd2cec4fd00e4f4f1a5b23c72cf4701d
dustinmoorman <dustin.moorman@gmail.com>
2013-11-18 13:31:27 -0600

    Launching Sheds Category.

--- module/TbAdmin/src/TbAdmin/Controller/IndexController.php
+++ module/TbAdmin/src/TbAdmin/Controller/IndexController.php
@@ -29,13 +29,13 @@ class IndexController extends AbstractActionController
         $em = $this->getEntityManager();
         $PtgTbCategory = new \PtgTbCategory\Entity\Category();
 
-        $PtgTbCategory->description = 'Mother Nature can take her toll on your vehicle. Rest assured knowing your car is safe under our extremely durable carports. Coming in such a wide variety, at an affordable price, there is no reason to not take advantage of these products, today. From being 100% American made, to offering a 10 year warranty, we are positive we can build the perfect carport for your needs. Contact us today for pricing and details.';
+        $PtgTbCategory->description = 'Having new toys for work or the farm is fun, but storing them can be a hassle. Our sheds provide you with the best storage solution for your tractors, ATV’s, or bulldozers at an affordable price. From being 100% American made, to offering a 10 year warranty, we are positive we can build the perfect shed for your needs. Contact us today for pricing and details.';
 
-        $PtgTbCategory->title = "Carports";
-        $PtgTbCategory->image_directory = 'carports';
-        $PtgTbCategory->main_pic_src = 'aframe.jpg';
-        $PtgTbCategory->slug = "carports";
-        $PtgTbCategory->subdescription = "The affordable way to protect your vehicle from the elements.";
+        $PtgTbCategory->title = "Sheds";
+        $PtgTbCategory->image_directory = 'sheds';
+        $PtgTbCategory->main_pic_src = 'shed2.jpg';
+        $PtgTbCategory->slug = "sheds";
+        $PtgTbCategory->subdescription = "The best storage solution for your tractors, ATV's, and bulldozers.";
 
 
         $em->persist($PtgTbCategory);
