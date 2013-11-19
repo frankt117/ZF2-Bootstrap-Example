fe458aa01aa82d36dc91969384af4458a4e51c6c
dustinmoorman <dustin.moorman@gmail.com>
2013-11-19 10:30:23 -0600

    Adding inputs

--- module/TbAdmin/src/TbAdmin/Controller/CategoryController.php
+++ module/TbAdmin/src/TbAdmin/Controller/CategoryController.php
@@ -6,6 +6,8 @@ use Zend\View\Model\ViewModel;
 
 class CategoryController extends AbstractActionController
 {
+    protected $inputs = array();
+
     public function indexAction()
     {
         $viewModel = new ViewModel();
@@ -25,11 +27,83 @@ class CategoryController extends AbstractActionController
     }
 
     public function addAction(){
-        return new ViewModel();
+
+        $this->getTitleInput();
+        $this->getSlugInput();
+        $this->getImgDirInput();
+        $this->getMainPicNameInput();
+        $this->getSubDescriptionInput();
+        $this->getDescriptionInput();
+
+        return new ViewModel($this->inputs);
     }
 
     public function editAction(){
-        return new ViewModel();
+
+        $this->getTitleInput();
+        $this->getSlugInput();
+        $this->getImgDirInput();
+        $this->getMainPicNameInput();
+        $this->getSubDescriptionInput();
+        $this->getDescriptionInput();
+
+        return new ViewModel($this->inputs);
     }
+
+    public function getTitleInput(){
+        $this->inputs[] = '<div class="form-group">
+                <label for="title" class="col-sm-2 control-label">Title</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Barns">
+                </div>
+            </div>';
+    }
+
+    public function getSlugInput(){
+        $this->inputs[] = '<div class="form-group">
+                <label for="slug" class="col-sm-2 control-label">Slug</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="slug" name="slug" placeholder="barns">
+                </div>
+            </div>';
+    }
+
+    public function getImgDirInput(){
+        $this->inputs[] = '<div class="form-group">
+                <label for="image_directory" class="col-sm-2 control-label">Image Directory</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="image_directory" name="image_directory" placeholder="barns">
+                </div>
+            </div>';
+    }
+
+    public function getMainPicNameInput(){
+        $this->inputs[] = '<div class="form-group">
+                <label for="main_pic_name" class="col-sm-2 control-label">Main Image Name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="main_pic_name" name="main_pic_name" placeholder="barn1.jpg">
+                </div>
+            </div>';
+    }
+
+    public function getSubDescriptionInput(){
+        $this->inputs[] = '<div class="form-group">
+                <label for="subdescription" class="col-sm-2 control-label">Sub Description</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="subdescription" name="subdescription" placeholder="Affordable and built durable for your horses, livestock, and RV.">
+                </div>
+            </div>';
+    }
+
+    public function getDescriptionInput(){
+        $this->inputs[] = '<div class="form-group">
+                <label for="description" class="col-sm-2 control-label">Description</label>
+                <div class="col-sm-10">
+                    <textarea class="form-control" rows="3" id="description" name="description"
+                    value="Easily being our most popular items, our barns accommodate everything from horses, to livestock, to even RV’s. With our wide variety and multiple sizes, we can build the ultimate barn to your specifications. From being 100% American made, to offering a 10 year warranty, we are positive we can build the perfect barn for your needs. Contact us today for pricing and details.">
+                </div>
+            </div>';
+    }
+
 
 }
