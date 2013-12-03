e9badac375df8bb3be6d2fad6d0c1688e07930d5
dustinmoorman <dustin.moorman@gmail.com>
2013-12-03 13:27:21 -0600

    lets see if this shyt worx

--- module/TbAdmin/src/TbAdmin/Controller/ProductController.php
+++ module/TbAdmin/src/TbAdmin/Controller/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends AbstractActionController
     }
 
     public function findAction(){
-        $this->getSelectCategoryInput();
+        $this->getSelectProductInput();
         $this->getNextButton();
 
         return new ViewModel(array('inputs' => $this->inputs));
@@ -45,61 +45,67 @@ class ProductController extends AbstractActionController
     }
 
     protected function saveData($post_data){
-        if ($post_data['select_category']){ //edit mode
-            $result = $this->editCategory($post_data);
+        if ($post_data['select_product']){ //edit mode
+            $result = $this->editProduct($post_data);
         }
         else {                              // new addition
-            $result = $this->addCategory($post_data);
+            $result = $this->addProduct($post_data);
         }
         return $result;
     }
 
-    protected function editCategory($post_data){
+    protected function editProduct($post_data){
         $em = $this->getEntityManager();
-        $result = 'Edit Category Failed.';
+        $result = 'Edit Product Failed.';
         if($post_data['add_edit_password'] == 'M@dMoney'){
-            /**@var \PtgTbCategory\Entity\Category $PtgTbCategory */
-            $PtgTbCategory = $em->getRepository('\PtgTbCategory\Entity\Category')->findOneBy(
-                array('id' => $post_data['select_category'])
+            /**@var \PtgTbProduct\Entity\Product $PtgTbProduct */
+            $PtgTbProduct = $em->getRepository('\PtgTbProduct\Entity\Product')->findOneBy(
+                array('id' => $post_data['select_product'])
             );
 
-            $PtgTbCategory->title = $post_data['title'];
-            $PtgTbCategory->slug = $post_data['slug'];
-            $PtgTbCategory->image_directory = $post_data['image_directory'];
-            $PtgTbCategory->main_pic_src = $post_data['main_pic_name'];
-            $PtgTbCategory->subdescription = $post_data['subdescription'];
-            $PtgTbCategory->description = $post_data['description'];
+            $PtgTbProduct->name = $post_data['name'];
+            $PtgTbProduct->slug = $post_data['slug'];
+            $PtgTbProduct->price = $post_data['price'];
+            $PtgTbProduct->image_directory = $post_data['image_directory'];
+            $PtgTbProduct->main_pic_src = $post_data['main_pic_name'];
+            $PtgTbProduct->subdescription = $post_data['subdescription'];
+            $PtgTbProduct->description = $post_data['description'];
+            $PtgTbProduct->main_category = $post_data['main_category'];
+            $PtgTbProduct->sub_category = $post_data['sub_category'];
 
-            $em->persist($PtgTbCategory);
+            $em->persist($PtgTbProduct);
             $em->flush();
 
-            $result = "Category (#" . $PtgTbCategory->id . ") " . $PtgTbCategory->title . " Updated Successfully.";
+            $result = "Product (#" . $PtgTbProduct->id . ") " . $PtgTbProduct->name . " Updated Successfully.";
         }
 
         return $result;
 
     }
 
-    protected function addCategory($post_data){
+    protected function addProduct($post_data){
 
         $em = $this->getEntityManager();
-        $result = 'Add Category Failed.';
+        $result = 'Add Product Failed.';
 
         if($post_data['add_edit_password'] == 'M@dMoney'){
-            /**@var \PtgTbCategory\Entity\Category $PtgTbCategory */
-            $PtgTbCategory = new \PtgTbCategory\Entity\Category();
+            /**@var \PtgTbProduct\Entity\Product $PtgTbProduct */
+            $PtgTbProduct = new \PtgTbProduct\Entity\Product();
 
-            $PtgTbCategory->title = $post_data['title'];
-            $PtgTbCategory->slug = $post_data['slug'];
-            $PtgTbCategory->image_directory = $post_data['image_directory'];
-            $PtgTbCategory->main_pic_src = $post_data['main_pic_name'];
-            $PtgTbCategory->subdescription = $post_data['subdescription'];
-            $PtgTbCategory->description = $post_data['description'];
+            $PtgTbProduct->name = $post_data['name'];
+            $PtgTbProduct->slug = $post_data['slug'];
+            $PtgTbProduct->price = $post_data['price'];
+            $PtgTbProduct->image_directory = $post_data['image_directory'];
+            $PtgTbProduct->main_pic_src = $post_data['main_pic_name'];
+            $PtgTbProduct->subdescription = $post_data['subdescription'];
+            $PtgTbProduct->description = $post_data['description'];
+            $PtgTbProduct->main_category = $post_data['main_category'];
+            $PtgTbProduct->sub_category = $post_data['sub_category'];
 
-            $em->persist($PtgTbCategory);
+            $em->persist($PtgTbProduct);
             $em->flush();
 
-            $result = "Category (#" . $PtgTbCategory->id . ") " . $PtgTbCategory->title . " Added Successfully.";
+            $result = "Product (#" . $PtgTbProduct->id . ") " . $PtgTbProduct->name . " Added Successfully.";
         }
 
         return $result;
@@ -107,12 +113,14 @@ class ProductController extends AbstractActionController
 
     public function addAction(){
 
-        $this->getTitleInput();
+        $this->getNameInput();
         $this->getSlugInput();
         $this->getImgDirInput();
         $this->getMainPicNameInput();
         $this->getSubDescriptionInput();
         $this->getDescriptionInput();
+        $this->getSelectMainCategoryInput();
+        $this->getSelectSubCategoryInput();
         $this->getAddEditPasswordInput();
         $this->getSaveButton();
 
@@ -127,16 +135,18 @@ class ProductController extends AbstractActionController
         if($request->isPost()){
 
             $post_data = $request->getPost();
-            $c   = $em->getRepository('\PtgTbCategory\Entity\Category')->findOneBy(array('id' => $post_data['select_category']));
-            if($c instanceof \PtgTbCategory\Entity\Category){
+            $c   = $em->getRepository('\PtgTbProduct\Entity\Product')->findOneBy(array('id' => $post_data['select_product']));
+            if($c instanceof \PtgTbProduct\Entity\Product){
 
-                $this->getHiddenCategoryIdInput($c->id);
-                $this->getTitleInput($c->title);
+                $this->getHiddenProductIdInput($c->id);
+                $this->getNameInput($c->name);
                 $this->getSlugInput($c->slug);
                 $this->getImgDirInput($c->image_directory);
                 $this->getMainPicNameInput($c->main_pic_src);
                 $this->getSubDescriptionInput($c->subdescription);
                 $this->getDescriptionInput($c->description);
+                $this->getSelectMainCategoryInput($c->main_category);
+                $this->getSelectSubCategoryInput($c->sub_category);
                 $this->getAddEditPasswordInput();
                 $this->getSaveButton();
 
@@ -147,21 +157,69 @@ class ProductController extends AbstractActionController
         return new ViewModel(array('inputs' => $this->inputs));
     }
 
-    protected function getHiddenCategoryIdInput($cat_id) {
-        $this->inputs[] = "<input type='hidden' name='select_category' id='select_category' value='$cat_id' />";
+    protected function getHiddenProductIdInput($cat_id) {
+        $this->inputs[] = "<input type='hidden' name='select_product' id='select_product' value='$cat_id' />";
     }
 
-    public function getSelectCategoryInput(){
+    public function getSelectMainCategoryInput($v = ''){
         $em = $this->getEntityManager();
 
         $select = '<div class="form-group">
-                <label for="select_category" class="col-sm-2 control-label">Select Category</label>
+                <label for="select_main_category" class="col-sm-2 control-label">Select Main Category</label>
                 <div class="col-sm-10">
-                    <select class="form-control" id="select_category" name="select_category">
+                    <select class="form-control" id="select_main_category" name="main_category">
                     ';
 
-            foreach($em->getRepository('\PtgTbCategory\Entity\Category')->findAll() as $category){
-                $select .= '<option value="' . $category->id . '">' . $category->title .'</option>';
+        foreach($em->getRepository('\PtgTbCategory\Entity\Category')->findAll() as $category){
+            $select .= '<option ';
+
+            $select .= $v == $category->id ? ' selected ' : '';
+
+            $select .='value="' . $category->id . '">' . $category->title .'</option>';
+        }
+
+        $select .=    '</select>
+                </div>
+            </div>';
+
+        $this->inputs[] = $select;
+    }
+
+    public function getSelectSubCategoryInput($v =''){
+        $em = $this->getEntityManager();
+
+        $select = '<div class="form-group">
+                <label for="select_sub_category" class="col-sm-2 control-label">Select Subcategory</label>
+                <div class="col-sm-10">
+                    <select class="form-control" id="select_sub_category" name="sub_category">
+                    ';
+
+        foreach($em->getRepository('\PtgTbCategory\Entity\Category')->findAll() as $category){
+            $select .= '<option ';
+
+            $select .= $v == $category->id ? ' selected ' : '';
+
+            $select .='value="' . $category->id . '">' . $category->title .'</option>';
+        }
+
+        $select .=    '</select>
+                </div>
+            </div>';
+
+        $this->inputs[] = $select;
+    }
+
+    public function getSelectProductInput(){
+        $em = $this->getEntityManager();
+
+        $select = '<div class="form-group">
+                <label for="select_product" class="col-sm-2 control-label">Select Product</label>
+                <div class="col-sm-10">
+                    <select class="form-control" id="select_product" name="select_product">
+                    ';
+
+            foreach($em->getRepository('\PtgTbProduct\Entity\Product')->findAll() as $product){
+                $select .= '<option value="' . $product->id . '">' . $product->name .'</option>';
             }
 
         $select .=    '</select>
@@ -171,26 +229,26 @@ class ProductController extends AbstractActionController
         $this->inputs[] = $select;
     }
 
-    public function getTitleInput($v = ''){
-        $title = '<div class="form-group">
-                <label for="title" class="col-sm-2 control-label">Title</label>
+    public function getNameInput($v = ''){
+        $name = '<div class="form-group">
+                <label for="name" class="col-sm-2 control-label">Name</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Barns" ';
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Horse Barn" ';
 
-        if($v) $title .= 'value ="' . $v .'" ';
+        if($v) $name .= 'value ="' . $v .'" ';
 
-        $title .= ' >
+        $name .= ' >
                 </div>
             </div>';
 
-        $this->inputs[] = $title;
+        $this->inputs[] = $name;
     }
 
     public function getSlugInput($v = ''){
         $slug = '<div class="form-group">
                 <label for="slug" class="col-sm-2 control-label">Slug</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="slug" name="slug" placeholder="barns"';
+                    <input type="text" class="form-control" id="slug" name="slug" placeholder="horse-barn"';
         if($v) $slug .= 'value ="' . $v .'" ';
 
         $slug .= ' >
@@ -218,7 +276,7 @@ class ProductController extends AbstractActionController
         $main_pic = '<div class="form-group">
                 <label for="main_pic_name" class="col-sm-2 control-label">Main Image Name</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="main_pic_name" name="main_pic_name" placeholder="barn1.jpg"';
+                    <input type="text" class="form-control" id="main_pic_name" name="main_pic_name" placeholder="horse-barn1.jpg"';
         if($v) $main_pic .= 'value ="' . $v .'" ';
 
         $main_pic .= ' >
@@ -232,7 +290,7 @@ class ProductController extends AbstractActionController
         $subdesc = '<div class="form-group">
                 <label for="subdescription" class="col-sm-2 control-label">Sub Description</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="subdescription" name="subdescription" placeholder="Affordable and built durable for your horses, livestock, and RV."';
+                    <input type="text" class="form-control" id="subdescription" name="subdescription" placeholder="The perfect mix of protection and western style for your horse barning needs son!"';
         if($v) $subdesc .= 'value ="' . $v .'" ';
 
         $subdesc .= ' >
